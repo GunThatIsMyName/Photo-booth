22f43a7493cb32328cd66ff513a7a4ec9379189a
Junhyun <mnmthatisus@gmail.com>
2022-01-28 22:42:40 +0900

    giphy simple pagination

--- src/App.js
+++ src/App.js
@@ -1,24 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import { API_ENDPOINT, API_KEY } from "./utils/Api";
 
 function App() {
+  const [list,setList]=useState([]);
+  const [count,setCount]=useState(0);
+
+
+  const getGifty=async()=>{
+    const response = await fetch(`${API_ENDPOINT}/trending?api_key=${API_KEY}&limit=3&offset=${count}`);
+    const {data} = await response.json();
+
+    const getData = data.map(item=>{
+      const {id,title,images}=item;
+      const {original:{url:image}}=images;
+      return {id,title,image};
+    })
+    setList(prev=>[...prev,...getData]);
+  }
+  
+  useEffect(()=>{
+    getGifty();
+  },[count])
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>Giphy</h1>
+
+      {list.map(item=>{
+        const {id,image,title}=item;
+        return <div key={id} >
+          <img src={image} alt={title} />
+          <h2>{title}</h2>
+        </div>
+      })}
+
+      <button onClick={()=>setCount(prev=>prev+3)} >Pagination</button>
+    </>
   );
 }
 
---
+++ src/utils/Api.js
@@ -0,0 +1,3 @@
+export const API_ENDPOINT ="https://api.giphy.com/v1/gifs";
+
+export const API_KEY ="kRvmeeIo89cs7bGvX5Hpcpy8ahAQ7KRm";
