eae1a58620b8fe8c155333695719c3bb23cc672f
Junhyun <mnmthatisus@gmail.com>
2022-02-02 23:13:11 +0900

    extra features

--- src/utils/Api.js
+++ src/utils/Api.js
@@ -1,7 +1,7 @@
 export const API_ENDPOINT ="https://api.giphy.com/v1/gifs";
 
-export const API_KEY ="kRvmeeIo89cs7bGvX5Hpcpy8ahAQ7KRm";
+export const API_KEY =process.env.REACT_APP_GIPHYAPI_KEY;
 
 export const API_ENDPOINT2 = "https://api.unsplash.com"
 
-export const CLIENT_ID = "vnNqxLzi0tWXKxhsb28XK-jQZuCQucJ-YMt-NZWus1I"
+export const CLIENT_ID = process.env.REACT_APP_UNSPLASH_KEY;
